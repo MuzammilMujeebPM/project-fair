4a1ea92ccb89ac758e75b62ee25efa9205f82768
MuzammilMujeebPM <muzammilmujeebpm24@gmail.com>
2024-12-12 11:34:43 +0530

    second msg

--- src/pages/Pnf.jsx
+++ src/pages/Pnf.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import img from '../assets/error.png'
 import { Link } from 'react-router-dom'
 
 const Pnf = () => {
   return (
     <div style={{height:'100vh'}} className='d-flex justify-content-center align-items-center flex-column'>
       <h2>404</h2>
-      <img src={img} alt="" />
       <h1>Look Like You're Lost</h1>
       <p>The page your looking for is not available</p>
       <Link to={'/'} className="btn btn-warning">Go To Home</Link>
